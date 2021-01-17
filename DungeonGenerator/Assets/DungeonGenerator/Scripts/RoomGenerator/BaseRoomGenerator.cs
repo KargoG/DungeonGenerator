d9433ef2954803801c1f858e10d8f507d9611c53
c.fedrau@web.de <>
2021-01-17 21:42:05 +0100

    Implement room creation based on L-System

--- DungeonGenerator/Assets/DungeonGenerator/Scripts/RoomGenerator/BaseRoomGenerator.cs
+++ DungeonGenerator/Assets/DungeonGenerator/Scripts/RoomGenerator/BaseRoomGenerator.cs
@@ -1,39 +1,155 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using DungeonGenerator;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
-public class BaseRoomGenerator : MonoBehaviour
+public class BaseRoomGenerator : MonoBehaviour, IRoomCreator
 {
     [SerializeField] private float _boxSize = 10;
     [SerializeField] private float _sizeChangePerIteration = 0.6f;
-    private Mesh _toShow;
 
+    private Dictionary<OperationSymbols, List<Tuple<Vector3, int>>> _anchorPoints = new Dictionary<OperationSymbols, List<Tuple<Vector3, int>>>();
+
+    private Mesh _toShow;
     private List<Bounds> _RectsInMesh = new List<Bounds>();
 
-    // Start is called before the first frame update
-    void Start()
+    public void CreateRoom(DungeonRoom dungeonRoom) 
     {
-        _toShow = new Mesh();
-        //_toShow = CubeGenerator.GenerateCube(10, 3, new Vector3(0, 0, 0));
-        //_RectsInMesh.Add(new Bounds(new Vector3(0, 0, 0), new Vector3(20, 20, 20) * 0.99f));
+        _toShow = CubeGenerator.GenerateCube(_boxSize * Mathf.Pow(_sizeChangePerIteration, 0), 3, new Vector3());
+        PrepareAnchors();
 
-        AddCubeAtAnchor(0);
-        AddCubeAtAnchor(1);
+        List<OperationSymbols> instructions = InstructionGenerator.GenerateInstructions(2);
+
+        foreach (OperationSymbols instruction in instructions)
+        {
+            AddCubeAtAnchor(instruction);
+        }
 
         GetComponent<MeshFilter>().mesh = _toShow;
     }
 
-    private void AddCubeAtAnchor(int iteration) // TODO calculate iteration in anchor method
+    public void SetUpConnections(GameObject[] roomsToConnectTo)
     {
-        Vector3 anchorPos = new Vector3(10, 10, 10); // TODO replace for real anchors
+        print("miau");
+    }
 
-        if(iteration == 0)
-            anchorPos = new Vector3(0, 0, 0); // TODO remove later
+    private void AddCubeAtAnchor(OperationSymbols position)
+    {
+        Tuple<Vector3, int> anchorPos = GetAnchor(position);
 
-        Mesh secondMesh = CubeGenerator.GenerateCube(_boxSize * Mathf.Pow(_sizeChangePerIteration, iteration), 3, anchorPos);
+        Vector3 newCubeSize = new Vector3(_boxSize, _boxSize, _boxSize) *
+                              Mathf.Pow(_sizeChangePerIteration, anchorPos.Item2);
 
-        AddShownMesh(ref secondMesh, new Bounds(anchorPos, new Vector3(20, 20, 20) * Mathf.Pow(_sizeChangePerIteration, iteration) * 0.99f));
+        Mesh secondMesh = CubeGenerator.GenerateCube(_boxSize * Mathf.Pow(_sizeChangePerIteration, anchorPos.Item2), 3, anchorPos.Item1);
+
+        AddAnchor(position, anchorPos.Item1, newCubeSize, anchorPos.Item2 + 1);
+        AddShownMesh(ref secondMesh, new Bounds(anchorPos.Item1, newCubeSize * 2 * 0.99f));
+    }
+
+    private Tuple<Vector3, int> GetAnchor(OperationSymbols position)
+    {
+        int anchorIndex = Random.Range(0, _anchorPoints[position].Count);
+        Tuple<Vector3, int> anchorPos = _anchorPoints[position][anchorIndex];
+        _anchorPoints[position].RemoveAt(anchorIndex);
+
+        return anchorPos;
+    }
+
+    private void AddAnchor(OperationSymbols anchorPosition, Vector3 center, Vector3 size, int depth)
+    {
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(size.x, size.y, 0), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(size.x, -size.y, 0), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(-size.x, size.y, 0), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(-size.x, -size.y, 0), depth));
+
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(0, size.y, size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(0, size.y, -size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(0, -size.y, size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(0, -size.y, -size.z), depth));
+
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(size.x, 0, size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(size.x, 0, -size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(-size.x, 0, size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(-size.x, 0, -size.z), depth));
+
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(size.x, size.y, size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(size.x, size.y, -size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(size.x, -size.y, size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(size.x, -size.y, -size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(-size.x, size.y, size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(-size.x, size.y, -size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(-size.x, -size.y, size.z), depth));
+        _anchorPoints[anchorPosition].Add(new Tuple<Vector3, int>(center + new Vector3(-size.x, -size.y, -size.z), depth));
+
+        RemoveOverlappingAnchors(anchorPosition);
+
+        switch (anchorPosition)
+        {
+            case OperationSymbols.XY:
+                break;
+            case OperationSymbols.XMY:
+                break;
+            case OperationSymbols.MXY:
+                break;
+            case OperationSymbols.MXMY:
+                break;
+            case OperationSymbols.YZ:
+                break;
+            case OperationSymbols.YMZ:
+                break;
+            case OperationSymbols.MYZ:
+                break;
+            case OperationSymbols.MYMZ:
+                break;
+            case OperationSymbols.ZX:
+                break;
+            case OperationSymbols.ZMX:
+                break;
+            case OperationSymbols.MZX:
+                break;
+            case OperationSymbols.MZMX:
+                break;
+            case OperationSymbols.XYZ:
+                break;
+            case OperationSymbols.XYMZ:
+                break;
+            case OperationSymbols.XMYZ:
+                break;
+            case OperationSymbols.XMYMZ:
+                break;
+            case OperationSymbols.MXYZ:
+                break;
+            case OperationSymbols.MXYMZ:
+                break;
+            case OperationSymbols.MXMYZ:
+                break;
+            case OperationSymbols.MXMYMZ:
+                break;
+        }
+    }
+
+    private void RemoveOverlappingAnchors(OperationSymbols anchorPosition)
+    {
+        List<Tuple<Vector3, int>> overlappingAnchors = new List<Tuple<Vector3, int>>();
+
+        for (int i = 0; i < _anchorPoints[anchorPosition].Count; i++)
+        {
+            foreach (Bounds bounds in _RectsInMesh)
+            {
+                if (bounds.Contains(_anchorPoints[anchorPosition][i].Item1))
+                {
+                    overlappingAnchors.Add(_anchorPoints[anchorPosition][i]);
+                }
+            }
+        }
+
+        foreach (Tuple<Vector3, int> overlappingAnchor in overlappingAnchors)
+        {
+            _anchorPoints[anchorPosition].Remove(overlappingAnchor);
+        }
     }
 
     private void AddShownMesh(ref Mesh secondMesh, Bounds newSquareBounds)
@@ -42,7 +158,6 @@ public class BaseRoomGenerator : MonoBehaviour
         List<Vector3> secondMeshVerts = new List<Vector3>(secondMesh.vertices);
         List<int> firstMeshTris = new List<int>(_toShow.triangles);
         List<int> secondMeshTris = new List<int>(secondMesh.triangles);
-
 
         List<int> indexesOfOverlappingVerts = new List<int>();
 
@@ -145,24 +260,38 @@ public class BaseRoomGenerator : MonoBehaviour
         _toShow.triangles = firstMeshTris.ToArray();
         _RectsInMesh.Add(newSquareBounds);
     }
+    
 
-    void OnDrawGizmosSelected()
+    void PrepareAnchors()
     {
-        if (!_toShow)
-            return;
-        //Gizmos.color = Color.blue;
-        //foreach (Vector3 vertex in toShow.vertices)
-        //{
-        //    Gizmos.DrawSphere(vertex, 0.05f);
-        //}
+        for (int i = 0; i < Enum.GetValues(typeof(OperationSymbols)).Length; i++)
+        {
+            _anchorPoints.Add((OperationSymbols)i, new List<Tuple<Vector3, int>>());
+        }
 
-        //Gizmos.color = Color.white;
-        //for (int i = 0; i < toShow.triangles.Length; i+=3)
-        //{
-        //    Gizmos.DrawLine(toShow.vertices[toShow.triangles[i]], toShow.vertices[toShow.triangles[i + 1]]);
-        //    Gizmos.DrawLine(toShow.vertices[toShow.triangles[i + 1]], toShow.vertices[toShow.triangles[i + 2]]);
-        //    Gizmos.DrawLine(toShow.vertices[toShow.triangles[i + 2]], toShow.vertices[toShow.triangles[i]]);
-        //}
+        _anchorPoints[OperationSymbols.XY].Add(new Tuple<Vector3, int>(new Vector3(_boxSize, _boxSize, 0), 1));
+        _anchorPoints[OperationSymbols.XMY].Add(new Tuple<Vector3, int>(new Vector3(_boxSize, -_boxSize, 0), 1));
+        _anchorPoints[OperationSymbols.MXY].Add(new Tuple<Vector3, int>(new Vector3(-_boxSize, _boxSize, 0), 1));
+        _anchorPoints[OperationSymbols.MXMY].Add(new Tuple<Vector3, int>(new Vector3(-_boxSize, -_boxSize, 0), 1));
+
+        _anchorPoints[OperationSymbols.YZ].Add(new Tuple<Vector3, int>(new Vector3(0, _boxSize, _boxSize), 1));
+        _anchorPoints[OperationSymbols.YMZ].Add(new Tuple<Vector3, int>(new Vector3(0, _boxSize, -_boxSize), 1));
+        _anchorPoints[OperationSymbols.MYZ].Add(new Tuple<Vector3, int>(new Vector3(0, -_boxSize, _boxSize), 1));
+        _anchorPoints[OperationSymbols.MYMZ].Add(new Tuple<Vector3, int>(new Vector3(0, -_boxSize, -_boxSize), 1));
+
+        _anchorPoints[OperationSymbols.ZX].Add(new Tuple<Vector3, int>(new Vector3(_boxSize, 0, _boxSize), 1));
+        _anchorPoints[OperationSymbols.ZMX].Add(new Tuple<Vector3, int>(new Vector3(-_boxSize, 0, _boxSize), 1));
+        _anchorPoints[OperationSymbols.MZX].Add(new Tuple<Vector3, int>(new Vector3(_boxSize, 0, -_boxSize), 1));
+        _anchorPoints[OperationSymbols.MZMX].Add(new Tuple<Vector3, int>(new Vector3(-_boxSize, 0, -_boxSize), 1));
+
+        _anchorPoints[OperationSymbols.XYZ].Add(new Tuple<Vector3, int>(new Vector3(_boxSize, _boxSize, _boxSize), 1));
+        _anchorPoints[OperationSymbols.XYMZ].Add(new Tuple<Vector3, int>(new Vector3(_boxSize, _boxSize, -_boxSize), 1));
+        _anchorPoints[OperationSymbols.XMYZ].Add(new Tuple<Vector3, int>(new Vector3(_boxSize, -_boxSize, _boxSize), 1));
+        _anchorPoints[OperationSymbols.XMYMZ].Add(new Tuple<Vector3, int>(new Vector3(_boxSize, -_boxSize, -_boxSize), 1));
+        _anchorPoints[OperationSymbols.MXYZ].Add(new Tuple<Vector3, int>(new Vector3(-_boxSize, _boxSize, _boxSize), 1));
+        _anchorPoints[OperationSymbols.MXYMZ].Add(new Tuple<Vector3, int>(new Vector3(-_boxSize, _boxSize, -_boxSize), 1));
+        _anchorPoints[OperationSymbols.MXMYZ].Add(new Tuple<Vector3, int>(new Vector3(-_boxSize, -_boxSize, _boxSize), 1));
+        _anchorPoints[OperationSymbols.MXMYMZ].Add(new Tuple<Vector3, int>(new Vector3(-_boxSize, -_boxSize, -_boxSize), 1));
     }
 
 }
---
+++ DungeonGenerator/Assets/DungeonGenerator/Scripts/RoomGenerator/InstructionGenerator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public enum OperationSymbols
+{
+    XY,
+    XMY,
+    MXY,
+    MXMY,
+    YZ,
+    YMZ,
+    MYZ,
+    MYMZ,
+    ZX,
+    ZMX,
+    MZX,
+    MZMX,
+    XYZ,
+    XYMZ,
+    XMYZ,
+    XMYMZ,
+    MXYZ,
+    MXYMZ,
+    MXMYZ,
+    MXMYMZ,
+}
+
+public class InstructionGenerator : ScriptableObject
+{
+    private static Dictionary<OperationSymbols, OperationSymbols[]> _reproductionRules;
+
+
+    public static List<OperationSymbols> GenerateInstructions(int reproductionIterations)
+    {
+        List<OperationSymbols> instructionString = GenerateStartString(5);
+
+        GenerateReproductionRules();
+
+        for (int i = 0; i < reproductionIterations; i++)
+        {
+            RunPass(ref instructionString);
+        }
+
+        return instructionString;
+    }
+
+    private static void RunPass(ref List<OperationSymbols> instructionString)
+    {
+        List<OperationSymbols> newGeneration = new List<OperationSymbols>();
+
+        foreach (OperationSymbols instruction in instructionString)
+        {
+            newGeneration.AddRange(_reproductionRules[instruction]);
+        }
+
+        instructionString = newGeneration;
+    }
+
+    private static List<OperationSymbols> GenerateStartString(int length)
+    {
+        List<OperationSymbols> startString = new List<OperationSymbols>();
+
+        for (int i = 0; i < length; i++)
+        {
+            startString.Add((OperationSymbols)Enum.GetValues(typeof(OperationSymbols)).GetValue(Random.Range(0, Enum.GetValues(typeof(OperationSymbols)).Length)));
+        }
+
+        return startString;
+    }
+
+    private static void GenerateReproductionRules()
+    {
+        if (_reproductionRules != null)
+            return;
+        _reproductionRules = new Dictionary<OperationSymbols, OperationSymbols[]>();
+
+        Array roomOperations = Enum.GetValues(typeof(OperationSymbols));
+        for (int i = 0; i < roomOperations.Length; i++)
+        {
+            OperationSymbols[] operations = new OperationSymbols[2];
+            for (int j = 0; j < operations.Length; j++)
+            {
+                OperationSymbols newOperation;
+                do
+                {
+                    newOperation = (OperationSymbols)roomOperations.GetValue(Random.Range(0, roomOperations.Length));
+                } while (newOperation == (OperationSymbols)roomOperations.GetValue(i));
+
+                operations[j] = newOperation;
+            }
+
+            _reproductionRules.Add((OperationSymbols)roomOperations.GetValue(i), operations);
+        }
+    }
+}
